86a5b010d99602d709a6c6d8a77c68729aa95033
talonMMA <xiangzhu0820@gmail.com>
2025-09-07 02:54:44 -0700

    First Commit

---
+++ src/main/java/com/talon/talon_101/Talon101Application.java
@@ -0,0 +1,13 @@
+package com.talon.talon_101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talon101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Talon101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/talon/talon_101/Talon101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talon.talon_101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talon101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
